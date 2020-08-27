e55006901215ef08409ba3bb5cbb0ee032b8c292
Patrick Tinsley <patrick.g.tinsley@gmail.com>
2020-08-27 18:46:55 -0400

    herro

---
+++ tri_pt.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath> 
+
+using namespace std;
+
+int main()
+{
+
+	// declare variables
+	int n;
+	double theta;
+
+	// prompt user for theta (degrees)
+	cout << "Input theta in degrees [double] : ";
+	cin >> theta;
+
+	// convert degrees to radians
+	double rads = (theta / 180.0) * M_PI;
+
+	// draw some triangles! 
+	int num_rows = 31;
+	
+	for (int r=0; r<num_rows; r++) { 
+	
+		// floor squashes down to nearest int, draw picture with r on y-axis/theta in top-left
+		for(int c=0; c <= floor( r * tan(rads) ); c++) { 
+
+			cout << "*";	
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
