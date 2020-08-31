92af72328b6103e17b9773c99b3be888495b3e17
Brian Tinsley <btinsley@usc.edu>
2020-08-31 16:11:14 -0700

    Create abundant.cpp

---
+++ abundant.cpp
@@ -0,0 +1,61 @@
+/********************************************************
+ * CS 103 PA 1: Abundant numbers
+ * Name: Brian Tinsley
+ * USC email: btinsley@USC.edu
+ * Comments (you want us to know):
+ *
+ * Description:
+ *   Displays the top 3 most "abundant" (see writeup)
+ *   numbers in the input sequence.  The input sequence
+ *   is assumed to be valid natural numbers separated
+ *   by spaces and ending with 0.
+ *
+ *   ABSOLUTELY NO ARRAY or `string`/`vector`/etc. usage
+ *   is allowed!
+
+ *   You may only declare: bool, int, or unsigned int
+ *   No library functions (beyond cin/cout) are allowed.
+ *
+ *   You may NOT modify any of the given template code.
+ *   This ensures our automated checkers can parse your
+ *   output.  Failure to follow this direction will lead
+ *   to a score of 0 on this part of the assignment.
+ ********************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // n1 is the number with the highest abundance, a1
+  // n2 is the number with the 2nd-highest abundance, a2
+  // n3 is the number with the 3rd-highest abundance, a3
+  unsigned int n1 = 0, n2 = 0, n3 = 0;
+  int a1 = 0, a2 = 0, a3 = 0;
+
+  // num_abundant counts how many numbers in the input
+  // sequence are abundant
+  int num_abundant = 0;
+
+  cout << "Enter a sequence of natural numbers, separated by spaces, ";
+  cout << "and ending with 0."<< endl;
+
+  /* Your code here */
+  
+  
+  
+  
+  
+    
+  /* End of your code */
+
+  cout << "Abundant number count: " << num_abundant << endl;
+  cout << "Top 3 most abundant numbers: " << endl;
+  cout << n1 << " : " << a1 << endl;
+  cout << n2 << " : " << a2 << endl;
+  cout << n3 << " : " << a3 << endl;
+
+
+  return 0;
+
+}
